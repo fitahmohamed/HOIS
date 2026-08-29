aa80eb919174cfbba7a9f9e7bc47da1656f1fe11
Aliaa Yasser <aliaayasser1986@gmail.com>
2026-08-29 15:42:48 +0300

    1

--- nthub_constructions/wizards/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import new_wbs_wizard
-from . import sub_task_wizard
--- nthub_constructions_tenders/wizards/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import new_version_wizard
-from . import import_tender_items_wizard
-from . import down_payment_invoice_wizard
-from . import jobcost_copy_choice_wizard
